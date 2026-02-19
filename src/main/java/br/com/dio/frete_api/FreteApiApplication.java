26991654aa4cb10881dae26a97717be58370d4e5
andreza.freitas <andreza1freitas@gmail.com>
2026-02-19 20:03:17 -0300

    feat: configuração inicial do projeto

---
+++ src/main/java/br/com/dio/frete_api/FreteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.frete_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/frete_api/FreteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.frete_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
